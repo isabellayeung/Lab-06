1e84d55584d34d0a022fab5de5c4b91b901b4109
isabellayeung <101486851+isabellayeung@users.noreply.github.com>
2022-03-14 18:33:30 -0400

    Add files via upload

---
+++ CalculatorAddAndSubtract.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class CalculatorAddAndSubtract {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+
+        // Declares variables
+        int optionChoice;
+        double valueOne;
+        double valueTwo;
+        double calculationResult;
+
+        // Calculator Menu Screen
+        System.out.println("Calculator Menu");
+        System.out.println("");
+        System.out.println("1. Addition");
+
+        // Prompts user to choose Addition
+        System.out.println("Which operation do you want to perform?");
+        optionChoice = scnr.nextInt();
+
+        // Prompts user to input values
+        System.out.print("Enter first operand: ");
+        valueOne = scnr.nextDouble();
+        System.out.print("Enter second operand: ");
+        valueTwo = scnr.nextDouble();
+
+        // Switch to choose Addition
+        switch (optionChoice) {
+            case 1:
+                // Adds first and second value together
+                calculationResult = valueOne + valueTwo;
+                System.out.println("The result of the operation is " + calculationResult + ".");
+                break;
+
+            default:
+                System.out.println("Error: Invalid selection!");
+        }
+
+    }
+}
